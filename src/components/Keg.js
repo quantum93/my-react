96cb107ce33e1e8780aecb8b2550b7b1862c0ff2
Student <example@example.com>
2019-09-11 11:17:00 -0700

    It add props using passing props feature

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Keg = () => <div></div>
+const Keg = (props) => { return (
+  <div>
+    <p>{props.kegName}</p>
+    <p>{props.brand}</p>
+    <hr/>
+  </div>
+)}
 
+Keg.propTypes = {
+  kegName: PropTypes.string,
+  brand: PropTypes.string,
+}
 export default Keg;
--- src/components/KegList.js
+++ src/components/KegList.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import Keg from './Keg';
 
-const KegList = () => <Keg />
+const KegList = () => { return (
+  <Keg
+    kegName="xxxx"
+    brand="yyyy"
+    // price=4.55
+    // alcoholContent= 2
+    // pints=20
+  />
+)}
 
 export default KegList;
