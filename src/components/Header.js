f4b223b0cc9dae660a1d6475b1f9f37d60a676fc
Student <example@example.com>
2019-09-11 13:59:08 -0700

    It add an image file on component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Logo from './logo192.png';
 
 const Header = () => {
   return (
     <div>
+      <img src={Logo}/>
       <h1>Tap Room</h1>
       <Link to="/">Home</Link> | <Link to="newkeg">Add new Keg</Link>
     </div>
