89839859ccdddceab4a1a368cfaa776f3c7a57af
Student <example@example.com>
2019-09-11 11:40:39 -0700

    It adds looping for data in JSX

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Keg = (props) => {
-  
+
   let myStyle = {
     backgroundColor: '#ecf0f1',
     fontFamily: 'sans-serif',
@@ -14,6 +14,9 @@ const Keg = (props) => {
   <div style={myStyle}>
     <p>{props.kegName}</p>
     <p>{props.brand}</p>
+    <p>{props.price}</p>
+    <p>{props.alcoholContent}</p>
+    <p>{props.pints}</p>
     <hr/>
   </div>
 )}
@@ -21,5 +24,8 @@ const Keg = (props) => {
 Keg.propTypes = {
   kegName: PropTypes.string,
   brand: PropTypes.string,
+  price: PropTypes.number,
+  alcoholContent: PropTypes.number,
+  pints: PropTypes.number,
 }
 export default Keg;
--- src/components/KegList.js
+++ src/components/KegList.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import Keg from './Keg';
 
-const KegList = () => { return (
-  <Keg
-    kegName="xxxx"
-    brand="yyyy"
-    // price=4.55
-    // alcoholContent= 2
-    // pints=20
-  />
+let masterKegList = [
+  {kegName: "Ginger Fixx", brand: "Lion Heart", price: 4.50, alcoholContent: 3, pints: 3},
+  {kegName: "Lemon Ginger", brand: "Humm", price: 3.50, alcoholContent: 0, pints: 86},
+  {kegName: "Original", brand: "Kevita Master Brew", price: 5.00, alcoholContent: 0, pints: 124},
+  {kegName: "Ginger Devil", brand: "Brew Dr.", price: 3.79, alcoholContent: 10, pints: 30},
+]
+const KegList = () => {
+
+  return (
+    <div>
+      <hr/>
+      {masterKegList.map((keg, index) =>
+        <Keg kegName={keg.kegName}
+             brand={keg.brand}
+             price={keg.price}
+             alcoholContent={keg.alcoholContent}
+             pints={keg.pints}
+             key={index} />
+      )}
+    </div>
 )}
 
 export default KegList;
